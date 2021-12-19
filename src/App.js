92069a1ce29b9ba6da4556e60d3186679f1e3c51
hi <sunkind0709@gmail.com>
2021-12-19 16:44:48 +0900

    04-setTimeout 값에대한 조건 선언

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import "./App.css"
 //그런다음에 useEffect 를 선언해준다. / 어떠한 효과를 줄것인 지에 대한 함수 선언.
 
 const App = () => {
-    const [weus, setWeus] = useState([])
+    const [weus, setWeus] = useState("")
     //안에 콘스트를 선언을 하고 해당 데이터 값들이 담길 그릇을 만들어준다.
     //임의로 지정해서 A 와 A-1 을 지정해서 만들어준다. 데이터가 담기는 그릇이 A-1 이라고 생각하면 된다.
 
     useEffect(() => {
-        console. log("hi")
+        setTimeout(() =>{
+            setWeus("bvhvbhjdvbhvbvhsdbh")
+        }, 6000)
     },[])
 
     return (
         <div>
             <h1>
-            {weus.length}
+            {weus}
             {/*데이터를 뿌려주기 위해서 함수를 선언해준다.*/}
+            {/*lengh 선언은 몇번이 사용되었는가에 대한 함수 추가이다.*/}
             </h1>
         </div>
     );
